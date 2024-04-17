d06acc991217b7245e78e01909f13e8ed92a5998
nexpamy <syafiqul@nexpasystem.com>
2024-04-17 13:12:24 +0800

    changed

--- gpio_example.py
+++ gpio_example.py
@@ -5,18 +5,18 @@ import time
 # Use the Broadcom SOC channel
 GPIO.setmode(GPIO.BCM)
 
-# Set up GPIO27 as an output
-GPIO.setup(27, GPIO.OUT)
+# This is your input pin (replace with your actual pin number)
+input_pin = 26
 
-# Set GPIO27 to high (3.3V)
-GPIO.output(27, GPIO.HIGH)
-time.sleep(1)  # Wait for 1 second
+# Set up the input pin with an initial state of DOWN
+GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-# Set GPIO27 to low (0V)
-GPIO.output(27, GPIO.LOW)
-
-# Clean up the GPIO state
-GPIO.cleanup()
-
-
-
+try:
+    while True:
+        # Wait for a rising edge on the input pin
+        GPIO.wait_for_edge(input_pin, GPIO.RISING)
+        print("Rising edge detected on pin {}".format(input_pin))
+        # Debounce the switch by waiting for 0.01 seconds
+        time.sleep(0.01)
+except KeyboardInterrupt:
+    GPIO.cleanup()  # Clean up GPIO settings before exiting
